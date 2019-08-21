722ee4657266501ce361965089e03bac0af35c5f
BLOC <36113405+furiousteam@users.noreply.github.com>
2019-08-21 14:19:38 +0200

    updated fork height
    updated fork height

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -156,7 +156,7 @@ const uint32_t UPGRADE_HEIGHT_MAX_BLOCK_SIZE                 = 200;
 const uint32_t UPGRADE_HEIGHT_V2                             = 50;
 const uint32_t UPGRADE_HEIGHT_V3                             = 100;
 const uint32_t UPGRADE_HEIGHT_V4                             = 200; // Upgrade height for CN-Lite Variant 1 switch.
-const uint32_t UPGRADE_HEIGHT_V5                             = 109600;
+const uint32_t UPGRADE_HEIGHT_V5                             = 31800;
 
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
@@ -175,7 +175,7 @@ const uint64_t FORK_HEIGHTS[] =
 	50, //1
 	100, //2
 	200, //3
-    	109600, //4
+    	31800, //4
 };
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
